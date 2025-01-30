db2c871adaca746802b70a30d29e740bd628cb88
Baltej30 <baltejsingh.22210141@viit.ac.in>
2025-01-30 11:42:12 +0530

    Create payment-model.js

---
+++ backend/models/payment-model.js
@@ -0,0 +1,11 @@
+const PaymentSchema = {
+    _id: "ObjectId", 
+    userId: "ObjectId", 
+    orderId: "ObjectId", 
+    amount: "Number", 
+    paymentMethod: "String", 
+    transactionId: "String", 
+    paymentStatus: "String", 
+    createdAt: "Date"
+  };
+  
